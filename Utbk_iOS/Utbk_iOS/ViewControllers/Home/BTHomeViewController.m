2504dcf7fe81f2483c86a6fed0c83b41b6908132
heyong <heyong@heyongdeMacBook-Pro.local>
2020-09-14 23:03:30 +0800

    首页布局处理

--- Utbk_iOS/Utbk_iOS/ViewControllers/Home/BTHomeViewController.m
+++ Utbk_iOS/Utbk_iOS/ViewControllers/Home/BTHomeViewController.m
@@ -8,7 +8,14 @@
 
 #import "BTHomeViewController.h"
 
-@interface BTHomeViewController ()
+@interface BTHomeViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UIView *bannerSuperView;
+@property (weak, nonatomic) IBOutlet UIImageView *adImageView;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UILabel *coinCount;//原始母币
+@property (weak, nonatomic) IBOutlet UILabel *convertCount;//折合(USDT)
+@property (strong, nonatomic) NSArray *datasource;
 
 @end
 
@@ -16,9 +23,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupLayout];
+    
     // Do any additional setup after loading the view from its nib.
 }
-
+- (void)setupLayout{
+    self.tableView.tableFooterView = [UIView new];
+    self.tableView.separatorColor = RGBOF(0xE8E8E8);
+    self.tableView.separatorInset = UIEdgeInsetsMake(0, 12, 0, 0);
+}
+#pragma mark buttonAction
+- (IBAction)buttonClickAction:(UIButton *)sender {
+}
+#pragma mark 主板区/涨幅榜点击事件
+- (IBAction)typeButtonClickAction:(UIButton *)sender {
+}
+#pragma mark tableDelegate  datasource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return [tableView dequeueReusableCellWithIdentifier:@"ccc"];
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 60.f;
+}
 /*
 #pragma mark - Navigation
 
