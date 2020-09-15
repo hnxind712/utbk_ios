5386547b7a51102223f9d0ece1242f21910fb647
heyong <heyong@heyongdeMacBook-Pro.local>
2020-09-15 23:01:40 +0800

    代码提交

---
+++ Utbk_iOS/Utbk_iOS/ViewControllers/Profile/BTShareWalletVC.m
@@ -0,0 +1,32 @@
+//
+//  BTShareWalletVC.m
+//  Utbk_iOS
+//
+//  Created by heyong on 2020/9/15.
+//  Copyright © 2020 HY. All rights reserved.
+//
+
+#import "BTShareWalletVC.h"
+
+@interface BTShareWalletVC ()
+
+@end
+
+@implementation BTShareWalletVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
